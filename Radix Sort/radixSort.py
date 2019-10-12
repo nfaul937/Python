4ce3c8dfaf12d46220ba4352b57900a57ccbf805
Nick Faul <nick.g.faul@gmail.com>
2019-10-11 20:36:30 -0500

    Update radixSort.py

--- Radix Sort/radixSort.py
+++ Radix Sort/radixSort.py
@@ -6,13 +6,13 @@ def countSort (arr,d):
     for j in range(0, len(arr)):
         count[int(arr[j][d])] = count[int(arr[j][d])] + 1
 
-    for i in range(1,max(arr)+1):
+    for i in range(1,int(max(arr))+1):
         count[i] = count[i] + count[i-1]
         #print(count)
 
     for j in range(len(arr)-1, -1,-1):
-        outArray[count[arr[j][d]] -1 ] = arr[j]
-        count[arr[j][d]] = count[arr[j][d]] -1
+        outArray[count[int(arr[j][d])] -1 ] = int(arr[j])
+        count[int(arr[j][d])] = count[int(arr[j][d])] -1
     
     return outArray
 
@@ -30,4 +30,4 @@ def radixSort (arr, d):
 
 array = [3,6,3,8,3,1,8,2,1]
 ans = radixSort(array, 1)
-#print(ans)
+print(ans)
