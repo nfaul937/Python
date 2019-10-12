60644abdb9c39beb60aade1feeeb1df7d230410c
Nick Faul <nick.g.faul@gmail.com>
2019-10-11 19:14:22 -0500

    Swapping files

--- Radix Sort/countSort.py
+++ Radix Sort/countSort.py
@@ -1,25 +1,21 @@
 def countSort (arr):
     #Initialize counting array
     count = [0 for i in range(0,max(arr)+1)]
-    outArray = [None for i in range(len(arr))]
-    print(outArray)
+    outArray = [None for i in range(0, len(arr))]
 
     for j in range(0, len(arr)):
         count[arr[j]] = count[arr[j]] + 1
-        print(count)
-
-    print()
 
     for i in range(1,max(arr)+1):
         count[i] = count[i] + count[i-1]
         #print(count)
 
-    for j in range(len(arr), 1,-1):
-        outArray[count[arr[j]]] = arr[j]
+    for j in range(len(arr)-1, -1,-1):
+        outArray[count[arr[j]] -1 ] = arr[j]
         count[arr[j]] = count[arr[j]] -1
-
-    print(outArray)
-
     
+    return outArray
+
 array = [3,6,3,8,3,1,8,2,10]
-countSort(array)
+ans = countSort(array)
+print(ans)
