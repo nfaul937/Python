8759091796d3ee2636907f96f5455896fc5bd64a
Nick Faul <nick.g.faul@gmail.com>
2019-10-14 14:59:07 -0500

    Completed rough draft radix sort

--- Radix Sort/radixSort.py
+++ Radix Sort/radixSort.py
@@ -1,12 +1,13 @@
-def countSort (arr,d):
+def RadCountSort (arr,d):
     #Initialize counting array
-    count = [0 for i in range(0,int(max(arr))+1)]
+    count = [0 for i in range(0,10)]
+    
     outArray = [None for i in range(0, len(arr))]
 
     for j in range(0, len(arr)):
         count[int(arr[j][d])] = count[int(arr[j][d])] + 1
 
-    for i in range(1,int(max(arr))+1):
+    for i in range(1,int(len(arr))+1):
         count[i] = count[i] + count[i-1]
         #print(count)
 
@@ -20,14 +21,15 @@ def radixSort (arr, d):
 
     strArr = [ str(_) for _ in arr ]
 
-    for i in range(0, d):
+    for i in range(d-1, -1, -1):
         print(i)
-        countSort(strArr,i)
+        sortedArray = RadCountSort(strArr,i)
+        strArr = [ str(_) for _ in sortedArray ]
+        
+    return RadSortedArray
 
 
 
-
-
-array = [3,6,3,8,3,1,8,2,1]
-ans = radixSort(array, 1)
+array = [22,63,34,96,37,16,80,27,10]
+ans = radixSort(array, 2)
 print(ans)
