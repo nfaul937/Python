90265ebb294714f076edc244783b7d57aa57502f
Nick Faul <nick.g.faul@gmail.com>
2019-10-15 19:38:28 -0500

    Update bucketSort-2.py

--- Radix Sort/bucketSort-2.py
+++ Radix Sort/bucketSort-2.py
@@ -16,17 +16,20 @@ def bucketSort(arr):
 
     buckets = [[] for _ in range(n)]
 
-    for i in range(1,n):
-       buckets[math.floor(n * arr[i])] = arr[i]
+    for i in range(0,n):
+       buckets[math.floor(n * arr[i])].insert(0,arr[i])
     
-    for i in range(0,n-1):
+    for i in range(0,n):
         iSort(buckets[i])
 
+    result = []
     for i in range(n):
-        result = result + buckets[i]
+        
+        result.extend(buckets[i])
 
     return result
 
 arr = [random.uniform(0,1) for j in range(10)]
 
 ans = bucketSort(arr)
+print(ans)
