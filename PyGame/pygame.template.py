1ed247f785ca0c7666829fa1a734e5ee376834b1
Nick Faul <nick.g.faul@gmail.com>
2019-10-21 23:10:50 -0500

    Pygame tutorial

---
+++ PyGame/pygame.template.py
@@ -0,0 +1,43 @@
+import pygame
+import random
+
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+
+#colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+
+#game loop
+
+running = True
+
+while running:
+    
+    #keep running at right speed
+    clock.tick(FPS)
+
+    #process input (events)
+    for event in pygame.event.get():
+        #check for close x
+        if (event.type == pygame.QUIT):
+            running = False
+    #update
+
+    #draw/render
+    screen.fill((BLACK))
+
+    #after drawing flip the display
+    pygame.display.flip()
+
+pygame.quit()
