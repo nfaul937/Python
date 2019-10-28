4b93d43cb3c4f749e538b300b6c98b6d3c2204b8
Nick Faul <nick.g.faul@gmail.com>
2019-10-27 19:53:12 -0500

    Added map and new movement

--- PyGame/TileBasedGame/main.py
+++ PyGame/TileBasedGame/main.py
@@ -4,6 +4,7 @@
 # Video link: https://youtu.be/3UxnelT9aCo
 import pygame as pg
 import sys
+from os import path
 from settings import *
 from sprites import *
 
@@ -17,16 +18,23 @@ class Game:
         self.load_data()
 
     def load_data(self):
-        pass
+        game_folder = path.dirname(__file__)
+        self.map_data = []
+        with open (path.join(game_folder, "map.txt"), 'rt') as f:
+            for line in f:
+                self.map_data.append(line)
 
     def new(self):
         # initialize all variables and do all the setup for a new game
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
-        self.player = Player(self, 10, 10)
-        for x in range(10, 20):
-            Wall(self, x, 5)
-
+        
+        for row, tiles in enumerate(self.map_data):
+            for col, tile in enumerate(tiles):
+                if tile == '1':
+                    Wall(self, col, row)
+                if tile == 'P':
+                    self.player = Player(self, col, row)
     def run(self):
         # game loop - set self.playing = False to end the game
         self.playing = True
@@ -61,17 +69,6 @@ class Game:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 self.quit()
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_ESCAPE:
-                    self.quit()
-                if event.key == pg.K_LEFT:
-                    self.player.move(dx=-1)
-                if event.key == pg.K_RIGHT:
-                    self.player.move(dx=1)
-                if event.key == pg.K_UP:
-                    self.player.move(dy=-1)
-                if event.key == pg.K_DOWN:
-                    self.player.move(dy=1)
 
     def show_start_screen(self):
         pass
