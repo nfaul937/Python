0e8620850287106a8cac7b81cfa3c5406a42cdb9
Nick Faul <nick.g.faul@gmail.com>
2019-10-29 16:47:33 -0500

    Add node class

---
+++ Node/Node.py
@@ -0,0 +1,19 @@
+
+
+class Node:
+    def __init__(self, parent=None, left=None, right=None):
+        self.parent = parent
+        self.left = left
+        self.right = right
+
+
+node1 = Node()
+node2 = Node('one','two', 'three')
+
+print(node1.left)
+print(node1.right)
+print(node1.parent)
+print()
+print(node2.left)
+print(node2.right)
+print(node2.parent)
