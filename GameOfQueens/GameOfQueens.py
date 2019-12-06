edfd72dd91d64f3daf0b639780c18d1bed920955
Nick Faul <nick.g.faul@gmail.com>
2019-12-05 23:29:00 -0600

    updated GoQ and boards

--- GameOfQueens/GameOfQueens.py
+++ GameOfQueens/GameOfQueens.py
@@ -1,15 +1,31 @@
+'''
+File: gameOfQueens.py
+Program author(s): Nick Faul
+ClassName: Analysis of Algorithims
+Assignment: Game of Queens
+Due date: 12/05/2019
 
+Brief description: 
+This program reads in a number of queens and uses it as the dimensions for the chessboard. The program will then take the board values in sequence as input.
+Once the inputs has been received the program will use patterns to determine the maximum number of queens that can fit on a board.
+The program will total the board values that are "under" the queens on the board and return it as output. 
+'''
+#Swaps values at index i and j in a given array
 def swap(arr, i, j):
 		arr[i], arr[j] = arr[j], arr[i]
 
+#Find the total value from the board based on the locations given.
 def total(board, locations):
     num = 0
 
     for i in range(0 , len(locations)):
-        num += board[i][locations[i] -1]
+    
+        num += int(board[i][locations[i] -1])
 
     return num
 
+#A function to create a board of size "q x q" where q is the number of queens from the input
+#Used in testing only
 def makeBoard(q):
     count = 1
     
@@ -22,13 +38,13 @@ queens = int(input())
 board = [[None for i in range(queens)] for j in range(queens)]
 
 locations = []
-makeBoard(queens)
-'''
+#makeBoard(queens)
+
+#Take the board values as input and place them in the board array
 for i in range(queens):
     line = input().split()
     for j in range(queens):
         board[j][i] = line[j]
-   ''' 
 
 mod = queens % 6
 
@@ -52,15 +68,11 @@ elif mod == 3:
     locations.append(1)
     locations.remove(3)
     locations.append(3)
-    
+
 else:
     for i in range(2, queens + 2, 2):
         locations.append(i)
     for i in range(1 , queens , 2):
         locations.append(i)
 
-
-print(locations)
-
-ans = total(board , locations)
-print(ans)
+print(total(board, locations))
